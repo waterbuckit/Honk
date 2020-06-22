5c71c5341d1fd5444b99a669054dbd7e85582623
waterbucket <adamroyal232@yahoo.com>
2020-06-22 13:44:03 +0100

    Name change

--- src/com/water/bucket/HonkInterpreter.java
+++ src/com/water/bucket/HonkInterpreter.java
@@ -4,11 +4,11 @@ import com.water.bucket.interpreter.Interpreter;
 
 import java.io.IOException;
 
-public class SimpleInterpreter {
+public class HonkInterpreter {
 
     public static void main(String[] args) throws IOException {
         if(args.length > 1) {
-            System.err.println("Argument error, usage: si [path to script]");
+            System.err.println("Argument error, usage: honk [path to script]");
             System.exit(1);
         } else if(args.length == 1){
             new Interpreter(args[0]);
